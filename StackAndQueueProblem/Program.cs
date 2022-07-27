712399058ac1e5b63bb281c609f6a92abce9757e
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-27 23:51:44 +0530

    Add project files.

---
+++ StackAndQueueProblem/Program.cs
@@ -0,0 +1,7 @@
+﻿using StackAndQueueProblem;
+
+LinkedListStack linkedListStack = new LinkedListStack();
+linkedListStack.Push(70);
+linkedListStack.Push(30);
+linkedListStack.Push(56);
+linkedListStack.Display();
